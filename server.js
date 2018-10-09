c56187071821aed7bdaad814145a97becb3a3ed6
cewfowler <cewfowler@gmail.com>
2018-10-09 09:14:41 -0400

    created routes.js

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ var express = require('express'),
     port = process.env.PORT || 8080;
     //var uri;
 
+//log requests to console
+app.use(morgan('dev'));
+
+//parse html info
+app.use(bodyParser.json());
+
+//load routes and pass app
+require('./routes/routes')(app);
+
 app.listen(port, function() {
   console.log('Listening on port ' + config.port);
 });
