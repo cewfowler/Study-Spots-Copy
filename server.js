bfe172fbba512cfac2b0a974d165c819d6661572
cewfowler <cewfowler@gmail.com>
2018-10-09 08:46:57 -0400

    added config file and routes folder

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     morgan = require('morgan'),
     mongoose = require('mongoose'),
+    config = require('./config'),
     //cookieParser = require('cookie-parser')
     port = process.env.PORT || 8080;
-
+    //var uri;
 
 app.listen(port, function() {
-  console.log('Listening on port ' + port);
-})
+  console.log('Listening on port ' + config.port);
+});
