55066ad57f67d2694c3f641e6346835db6c782ba
cewfowler <cewfowler@gmail.com>
2018-10-11 11:17:31 -0400

    first version of post new room route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,9 +4,21 @@ var mongoose = require('mongoose'),
 
 module.exports = function(app) {
 
-  app.post('/:bldgCode', function(req, res){
-      console.log("Posting new study spot");
+  // the only thing that should be passed in the request is the building bCode
+  // and the location/room number for the new spot
+  app.post('/:bCode', function(req, res){
+      console.log("Attempting to post new study spot");
 
+      var newSpot = req.spot;
+      
+      SpotModel.findOneAndUpdate({bldgCode: bCode},
+        {$push: {spots: newSpot}}, {new: true}, function(err, spot) {
+          if (err) {
+            console.log("Error updating building with code " + bCode);
+          }
+
+          res.status(201).send();
+        });
 
   });
 
