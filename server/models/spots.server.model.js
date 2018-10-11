4dc1bbf279346e44eb9ef04dc4a1a1f804a65c51
cewfowler <cewfowler@gmail.com>
2018-10-11 09:57:39 -0400

    added get route for homepage/map, organized files into new folders

---
+++ server/models/spots.server.model.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var spotsSchema = new Schema ({
+  bldgName: {
+    type: String,
+    required: true
+  },
+
+  bldgCode: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  coordinates: {
+    lattitude: {
+      type: Number,
+      required: true
+    },
+    longitude: {
+      type: Number,
+      required: true
+    }
+  },
+
+  spots: {
+    type: [Schema.types.mixed]
+  },
+
+  updatedAt: Date
+
+});
+
+//TODO: in order to add a spot to the array, must call
+// spots.markModified('spots');
+
+//TODO: make sure createdAt is initialized in the add/create study spot function
+//This function is meant to change the updated time to the save time
+spotsSchema.pre('save', function(next) {
+  var curTime = new Date;
+  this.updatedAt = curTime;
+
+  next();
+});
+
+// Use schema to instantiate a mongoose model
+var Spot = mongoose.model('Spot', spotsSchema);
+
+module.exports = Spot;
