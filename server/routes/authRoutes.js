27bae5c96397099f10f70831497c00edc283297e
cewfowler <cewfowler@gmail.com>
2018-11-07 13:23:49 -0500

    authentication login, register routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,17 @@
-var Users = require('../models/users.server.model.js');
+var Users = require('../models/users.server.model.js'),
+    passStrats = require('../../config/passport.js');
     //config = require('../../config/config'),
 
 module.exports = function(app) {
-  app.get('/user', function(req, res) {
+  app.get('/user/login', passport.authenticate('local-login', {
+    failureFlash: true
+  }), function(req, res) {
+
+  });
+
+  app.post('user/register', passport.authenticate('local-signup', {
+    failureFlash: true
+  }), function(req, res) {
 
   });
 }
