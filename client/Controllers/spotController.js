1495aaccbd5f6c7eed4aa2824c3b5442cfc3f67e
Danny Moolchand <dannyrmoolchand@ufl.edu>
2018-11-15 11:10:47 -0500

    n/a

--- client/Controllers/spotController.js
+++ client/Controllers/spotController.js
@@ -262,6 +262,14 @@ angular.module('spots').controller('SpotsController', ['$scope', 'Spots',
     });
     map.addControl(locationTracker, 'bottom-right');
 
+
+
+
+    var popupButton 
+
+
+
+
     //Event that checks for a click on the buildings layer
     map.on('click', 'spots', function (e) {
 
