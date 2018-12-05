1674d990c43e631db4b03317101f0dab21ea8f9b
Danny Moolchand <dannyrmoolchand@ufl.edu>
2018-12-04 22:11:47 -0500

    i think its working

--- client/Controllers/authentController.js
+++ client/Controllers/authentController.js
@@ -1,46 +1,48 @@
 angular.module('spots').controller('AuthenticationController', ['$scope', 'Spots', '$localStorage', 'userService',
-  function($scope, Spots, $localStorage, userService){
+  function ($scope, Spots, $localStorage, userService) {
 
-    $scope.$storage = $localStorage.$default({
-      email: ""
-    });
+      $scope.$storage = $localStorage.$default({
+        email: ""
 
-    if($scope.$storage.email != "") {
-      Spots.getUser($scope.$storage.email).then(function(user) {
+      });
+    
+
+    if ($scope.$storage.email != "") {
+      Spots.getUser($scope.$storage.email).then(function (user) {
         userService.user = user.data;
       });
     }
 
-    $scope.login = function(email, password){
+    $scope.login = function (email, password) {
       Spots.login(email, password);
       $scope.$storage.email = email;
     }
 
-    $scope.signup = function(email, password){
+    $scope.signup = function (email, password) {
       Spots.register(email, password);
       $scope.$storage.email = email;
     }
 
-    $scope.current = function(){
+    $scope.current = function () {
       console.log(userService.user);
     }
 
-    $scope.update = function(){
+    $scope.update = function () {
       Spots.updateUser(userService.user.email, userService.user);
     }
-    $scope.logout = function() {
+    $scope.logout = function () {
       Spots.logout();
       userService.user = "";
       $scope.$storage.email = "";
-      
+
     }
-    $scope.current = function() {
+    $scope.current = function () {
       console.log(userService.user);
     }
 
     $scope.isLogged = function () {
       // console.log("User Email is: " + $scope.$storage.email);
-      if($scope.$storage.email == ""){
+      if ($scope.$storage.email == "") {
         return false;
       }
       else {
@@ -48,5 +50,5 @@ angular.module('spots').controller('AuthenticationController', ['$scope', 'Spots
       }
     }
 
-    
+
   }]);
