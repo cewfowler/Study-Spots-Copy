d846fd4f007fce9b7bcb8b2f85ee6ff05dd5ce49
cewfowler <cewfowler@gmail.com>
2018-12-05 12:37:57 -0500

    d

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ module.exports = {
     uri: 'mongodb://cewfowler:Password1@ds037488.mlab.com:37488/study_spots',
   },
   usersdb: {
-    uri: 'mongodb://cewfowler:Password1@ds241493.mlab.com:41493/study_spots_users',
+    uri: 'mongodb://cewfowler:password1@ds033484.mlab.com:33484/study-spots-users',
   },
   port: 8080
 };
